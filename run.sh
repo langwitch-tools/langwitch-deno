4460a7e5e12e04be92165e16ea6986c56579ee77
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-27 20:41:27 +1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
+set -euo pipefail
+IFS=$'\n\t'
+
 if ! command -v nix-shell &> /dev/null
 then
     bash <(curl -L https://nixos.org/nix/install) --daemon
 fi
 
-if ! command -v deno &> /dev/null
-then
-    curl -fsSL https://deno.land/x/install/install.sh | sh
-fi
-
-nix-shell --run "deno run -A --unstable --no-check --reload https://github.com/langwitch-tools/langwitch-deno/raw/main/src/configs/interactive.ts" -p mpv
+nix-shell --run "deno cache --reload https://github.com/langwitch-tools/langwitch-deno/raw/main/src/configs/interactive.ts; deno run -A --unstable --no-check https://github.com/langwitch-tools/langwitch-deno/raw/main/src/configs/interactive.ts" -p deno mpv
