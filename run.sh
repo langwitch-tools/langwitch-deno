545410ea90ade2b45f4123baa35f2ac86681e511
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-27 20:32:09 +1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ if ! command -v nix-shell &> /dev/null
 then
     bash <(curl -L https://nixos.org/nix/install) --daemon
 fi
-nix-shell --run "deno run -A --unstable --no-check https://github.com/langwitch-tools/langwitch-deno/raw/main/src/configs/interactive.ts" -p deno mpv
+nix-shell --run "deno run -A --unstable --no-check --reload https://github.com/langwitch-tools/langwitch-deno/raw/main/src/configs/interactive.ts" -p deno mpv
