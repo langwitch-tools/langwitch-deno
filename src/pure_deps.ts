d322872e9a067655b07ef45bfaae3b2be744257d
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-27 20:30:45 +1200

    Update pure_deps.ts

--- src/pure_deps.ts
+++ src/pure_deps.ts
@@ -1,6 +1,6 @@
 export * as R from "https://esm.sh/ramda@0.28.0";
 export { default as Monet } from "https://esm.sh/monet@0.9.3";
-export { default as wu, type WuIterable } from "https://esm.sh/wu@2.1.0";
+export { default as wu } from "https://esm.sh/wu@2.1.0";
 export { default as fnv } from "https://esm.sh/fnv-plus@1.3.1";
 export * as Diff from "https://esm.sh/diff@5.1.0";
 export { immerable, produce } from "https://esm.sh/immer@9.0.15";
