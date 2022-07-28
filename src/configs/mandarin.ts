dc812279454ae6235632ddeed76850e0548f83b2
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-28 22:35:44 +1200

    Update mandarin.ts

--- src/configs/mandarin.ts
+++ src/configs/mandarin.ts
@@ -1,6 +1,6 @@
-import { start } from "https://raw.githubusercontent.com/langwitch-tools/langwitch-deno/main/src/external/config_helpers/start_session.ts";
+import { start } from "https://github.com/langwitch-tools/langwitch-deno/raw/main/src/external/config_helpers/start_session.ts";
 import { ExternalConfig } from "https://github.com/langwitch-tools/langwitch-deno/raw/main/src/external/config_helpers/ExternalConfig.ts";
-import { makeTTS } from "https://raw.githubusercontent.com/langwitch-tools/langwitch-deno/main/src/external/config_helpers/speech.ts";
+import { makeTTS } from "https://github.com/langwitch-tools/langwitch-deno/raw/main/src/external/config_helpers/speech.ts";
 import { cut } from "https://deno.land/x/jieba@v1.0.0/mod.ts";
 import pinyin from "https://deno.land/x/pinyin/mod.ts";
 
