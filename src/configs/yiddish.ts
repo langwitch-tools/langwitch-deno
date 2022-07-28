01a500a24831ea92bc5aa30797f46f214112b0f6
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-28 22:20:31 +1200

    Create yiddish.ts

---
+++ src/configs/yiddish.ts
@@ -0,0 +1,31 @@
+import { start } from "../../external/config_helpers/start_session.ts";
+import { ExternalConfig } from "../../external/config_helpers/ExternalConfig.ts";
+import { translit } from "https://archive.org/download/ii_20220728/translit/mod.ts"; 
+
+const transliterator = await translit("yi").then(m => m.transliterate);
+
+const config: ExternalConfig = {
+	language: "yiddish",
+
+	save_my_concepts_to_this_file: "yiddish-concepts.json",
+
+	get_sentences_from: [
+		{
+			url: `https://archive.org/download/langwitch_shortened/shortened_yi.txt`,
+			total: 120000,
+		},
+	],
+
+	show_progress_bar: true,
+
+	transliteration_options: {
+        convert_my_answers_to_this_before_marking: (s: string) => transliterator(s).toLowerCase(),
+        make_additional_data_from_sentence_using_this: (sentence: string) => ({
+            original: sentence
+        })
+    },
+
+	music: false,
+};
+
+start(config).run()
