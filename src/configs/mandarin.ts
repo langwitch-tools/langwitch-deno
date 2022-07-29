c525c7f101dcac38c6641e0ad806c774c3cdd01e
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-29 21:30:18 +1200

    Update mandarin.ts

--- src/configs/mandarin.ts
+++ src/configs/mandarin.ts
@@ -1,5 +1,5 @@
-import { start } from "../../external/config_helpers/start_session.ts";
-import { makeTTS } from "../../external/config_helpers/speech.ts";
+import { start } from "../external/config_helpers/start_session.ts";
+import { makeTTS } from "../external/config_helpers/speech.ts";
 import { cut } from "https://deno.land/x/jieba@v1.0.0/mod.ts";
 import pinyin from "https://deno.land/x/pinyin/mod.ts";
 
